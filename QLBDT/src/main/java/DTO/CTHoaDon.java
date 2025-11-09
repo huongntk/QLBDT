95d4d2a6d176ef54795192406126bdcbd9928e58
huongntk <147575609+huongntk@users.noreply.github.com>
2025-11-09 19:45:38 +0700

    Add files via upload

---
+++ QLBDT/src/main/java/DTO/CTHoaDon.java
@@ -0,0 +1,67 @@
+package DTO;
+
+public class CTHoaDon {
+    private int maHD;
+    private int maSP;
+    private int soLuong;
+    private double donGia;
+    private int thanhTien;
+
+    public CTHoaDon() {
+    }
+
+    public CTHoaDon(int maHD, int maSP, int soLuong, double donGia) {
+        this.maHD = maHD;
+        this.maSP = maSP;
+        this.soLuong = soLuong;
+        this.donGia = donGia;
+    }
+    
+    public CTHoaDon(int maHD, int maSP, int soLuong, double donGia, int thanhTien) {
+        this.maHD = maHD;
+        this.maSP = maSP;
+        this.soLuong = soLuong;
+        this.donGia = donGia;
+        this.thanhTien = thanhTien;
+    }
+
+    public int getMaHD() {
+        return maHD;
+    }
+
+    public void setMaHD(int maHD) {
+        this.maHD = maHD;
+    }
+
+    public int getMaSP() {
+        return maSP;
+    }
+
+    public void setMaSP(int maSP) {
+        this.maSP = maSP;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public double getDonGia() {
+        return donGia;
+    }
+
+    public void setDonGia(int donGia) {
+        this.donGia = donGia;
+    }
+
+    public int getThanhTien() {
+        return thanhTien;
+    }
+
+    public void setThanhTien(int thanhTien) {
+        this.thanhTien = thanhTien;
+    }
+}
